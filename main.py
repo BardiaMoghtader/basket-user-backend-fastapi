78a137b1e8660c2fc1976183f02b16f4e39988be
BardiaMoghtader <74041111+BardiaMoghtader@users.noreply.github.com>
2022-07-22 15:12:45 +0300

    cookie time out
    just adjusted the timeout of the cookie for authentication/login.

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ from model.models import *
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"# "e1d69c39d71f9fd11381fcfc0af4c01314d8c6d7f93740f89e62a707cfbc0ffd"#
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 2
 
 users = {
     'johndoe': 'secret'
@@ -126,7 +126,7 @@ async def login(user: OAuth2PasswordRequestForm = Depends()):
 
     token = manager.create_access_token(
         data=dict(sub=user.username),
-        expires=timedelta(minutes=1)
+        expires=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     )
     response = RedirectResponse(url=f"/user/{user.username}", status_code=status.HTTP_302_FOUND)
     manager.set_cookie(response, token)
